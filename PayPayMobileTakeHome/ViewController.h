8cf0956e6a119b51709a24fc318f6742f2f3d8b5
ptaneja <PTRepo>
2014-06-06 13:08:52 -0400

    Initial Commit

---
+++ PayPayMobileTakeHome/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PayPayMobileTakeHome
+//
+//  Created by Tringapps Inc on 6/6/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PayPayMobileTakeHome/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PayPayMobileTakeHome
+//
+//  Created by Tringapps Inc on 6/6/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
